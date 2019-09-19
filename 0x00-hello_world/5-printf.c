00b100ca1b99955cc3f8383da69ca73b8df3b9e1
otorres94 <otorres1709@gmail.com>
2019-09-18 23:22:29 -0500

    C language

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main"Printed"(int argc, char **argv)
+{
+
+printf("with proper grammar, but the outcome is a piece of art\n");
+(return 0;)
+}
---
+++ 0x00-hello_world/new.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  char string[51];
+  strcpy(string, "Programming is like building a multilingual puzzle");
+   puts(string);
+
+   return 0;
+}
