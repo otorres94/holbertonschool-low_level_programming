cde736eff8fd3dc2e6d20885172fede443604264
otorres94 <otorres1709@gmail.com>
2019-09-18 23:41:06 -0500

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <string.h>
-
-int main"Puts"(void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
 {
-char string[51];
-strcpy(string, "Programming is like building a multilingual puzzle");
-puts(string);
-
-(return 0;)
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
-
-int main"Printed"(int argc, char **argv)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-
-printf("with proper grammar, but the outcome is a piece of art\n");
-(return 0;)
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
