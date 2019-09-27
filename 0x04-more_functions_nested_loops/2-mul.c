70a74bcc6b12e569a8ca8d6b6541ca4d833b5ade
otorres94 <otorres1709@gmail.com>
2019-09-26 22:47:41 -0500

    C-More_Funtions

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
-
 /**
- * _isdigit - Check for digits (0 through 9)
- * @c: int type param
- * Return: 1 if digit, else 0
- */
-int _isdigit(int c)
+ ** mul - multiply a and b
+ ** @a: input a
+ ** @b: input b
+ ** Return: result
+ **/
+int mul(int a, int b)
 {
-	if (c > 47 && c < 58)
-		return (1);
-
-	return (0);
+int result;
+result = a * b;
+return (result);
 }
