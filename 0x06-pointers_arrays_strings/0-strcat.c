aa860cc3cac14b691b64ec6ed592ee4efd61ea58
otorres94 <otorres1709@gmail.com>
2019-10-03 13:59:29 -0500

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,45 +1,24 @@
 #include "holberton.h"
-#include <string.h>
-
 /**
- * _strlen - return length of a string
- *
- * @s: char type
- * Return:  length of string
+ * _strcat - Copy a string in the final of other string
+ * @dest: First string.
+ * @src: Second string that print at the final to the first
+ * Return: The first string.
  */
-
-int _strlen(char *s)
-{
-	int a;
-
-	for (a = 0; s[a] != '\0'; a++)
-	{
-
-	}
-	return (a);
-}
-/**
- * _strcat - function to concatnate strings
- *
- * @dest: destination for concatnation
- *
- * @src: source of string
- *
- * Return: dest
- */
-
 char *_strcat(char *dest, char *src)
 {
-	int dest_len;
-	int a;
+	int a, b, origin;
 
-	dest_len = _strlen(dest);
-
-	for (a = 0 ; src[a] != '\0' ; a++)
-		dest[dest_len + a] = src[a];
-
-	dest[dest_len + a] = '\0';
+	a = 0;
+	b = 0;
+	while (src[b] != '\0')
+		b++;
+	while (dest[a] != '\0')
+		a++;
+	for (origin = 0; origin < b; origin++)
+	{
+		dest[origin + a] = src[origin];
+	}
 
 	return (dest);
-
 }
